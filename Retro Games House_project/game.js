cd19324da4a1483521277fae7ead8081aea880af
irensula <irinawebdev@gmail.com>
2024-05-26 23:18:09 +0300

    Add files via upload

--- Retro Games House_project/game.js
+++ Retro Games House_project/game.js
@@ -6,9 +6,14 @@ function readJson() {
     .then((data) => {
         let div = document.getElementById("root");
         data.games.forEach(game => {
+            
             let element = document.createElement("p");
-            element.innerHTML = game.game_name.fi
-            div.appendChild(element)
+            element.innerHTML = game.game_name.fi;
+            div.appendChild(element);
+
+            let img = document.createElement('img');
+            img.src = game.game_image;
+            div.appendChild(img)
         });
     })
 }
